32892315c08648fae537c78340cbfe0409bd4690
zobiyars <zobiyarehmansheikh@gmail.com>
2026-09-25 19:18:36 +0500

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,17 @@
+x = 4
+y = 3 + x
+print(x + y)
+# this gives an output 11.
+
+a = 4
+b = 3
+c = "five"
+a = 2
+print(a,c)
+# this gives an output 2 five
+
+d = 4
+e = 3
+f = "five"
+print(d + f)
+# this gives an error bcz python can't add integer and string directly.
